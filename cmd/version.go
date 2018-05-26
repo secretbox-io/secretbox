2eaed8fa67f57e55a39d4aca8e8faa9ebc2c0ebc
Bryan Burke <bryan.t.burke@gmail.com>
2018-05-26 14:43:56 -0400

    set up build system

--- cmd/version.go
+++ cmd/version.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var Version string
+var CommitHash string
+var BuildTime string
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of secretbox",
+	Long:  `All software has versions. This is secretbox's.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version: %s\nCommit: %s\nBuild Time: %s\n", Version, CommitHash, BuildTime)
+	},
+}
--- secretbox.go
+++ secretbox.go
@@ -3,9 +3,19 @@ import (
 	"github.com/secretbox-io/secretbox/cmd"
 )
 
-// VERSION is the current build number as tag and short commit hash
-var VERSION = "XXX"
+// Version is the tagged build number
+var Version string
+
+// CommitHash is the current commit for the build
+var CommitHash string
+
+// BuildTime is the build time
+var BuildTime string
 
 func main() {
+	cmd.Version = Version
+	cmd.CommitHash = CommitHash
+	cmd.BuildTime = BuildTime
+
 	cmd.Execute()
 }
